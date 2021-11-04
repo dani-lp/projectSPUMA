bf0d73f6c3f9ad97ccb759e74c1284a009ab4700
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2021-11-04 23:07:50 +0100

    Base landing page

--- spumAPP/views.py
+++ spumAPP/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("index.html, home.html")
+    context = { 'loggedIn': False }
+    return render(request, "index.html", context)
 
 
 def login(request):
