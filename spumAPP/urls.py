f1bc5d9076aab090959a4f674099b6a297914956
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2021-11-04 18:02:30 +0100

    Views base

--- spumAPP/urls.py
+++ spumAPP/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('settings', views.settings, name='settings'),
+    path('plugins/notes/<int:plugin_id>', views.notes_plugins, name='notes_plugins'),
+    path('plugins/tasks/<int:plugin_id>', views.tasks_plugins, name='tasks_plugins'),
 ]
--- spumAPP/views.py
+++ spumAPP/views.py
@@ -1,4 +1,24 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Listado de departamentos")
+    return HttpResponse("index.html, home.html")
+
+
+def login(request):
+    return HttpResponse("login.html")
+
+
+def register(request):
+    return HttpResponse("register.html")
+
+
+def settings(request):
+    return HttpResponse("settings.html")
+
+
+def notes_plugins(request, plugin_id):
+    return HttpResponse("notes.html")
+
+
+def tasks_plugins(request, plugin_id):
+    return HttpResponse("tasks.html")
