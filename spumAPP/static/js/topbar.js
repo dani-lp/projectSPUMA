5d4bb010f986a7561d8ac7c1e0e02534fd783233
Dzl17 <danilau11@gmail.com>
2021-11-05 13:43:43 +0100

    Botones para menu de topbar

--- spumAPP/static/js/topbar.js
+++ spumAPP/static/js/topbar.js
@@ -2,13 +2,16 @@ let extended = false;
 
 const menuToggle = document.querySelector('.topbar-menu-toggle');
 const menuDropdown = document.querySelector('.topbar-menu-collapse');
+const menuButtons = document.querySelector('.menu-buttons');
 
 menuToggle.onclick = () => {
     extended = !extended;
 
     if (extended) {
         menuDropdown.style.height = '100%';
+        menuButtons.style.display = 'block';
     } else {
         menuDropdown.style.height = '0';
+        menuButtons.style.display = 'none';
     }
 }
