180021bdacbd54a9637e9209810673a8a29650e0
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2021-11-07 02:15:28 +0100

    Dropdown usuario base

--- spumAPP/static/js/topbar.js
+++ spumAPP/static/js/topbar.js
@@ -1,7 +1,7 @@
-let extended = false;
+let menuExtended = false;
 
 const processTopbarState = () => {
-    if (extended) {
+    if (menuExtended) {
         $('.topbar-menu-collapse').css('height', '100%');
         $('.topbar-collapse-container').css({ 
             opacity: 1, 
@@ -17,11 +17,36 @@ const processTopbarState = () => {
 }
 
 $('#topbar-menu-toggle').click(function() {
-    extended = !extended;
+    menuExtended = !menuExtended;
     processTopbarState();
 });
 
 $('.topbar-anchor').click(function() {
-    extended = false;
+    menuExtended = false;
     processTopbarState();
+});
+
+
+// User dropdown
+let userExtended = false;
+
+$('#topbar-user-toggle').click(function(e) {
+    userExtended = !userExtended;
+
+    if(userExtended) {
+        $('#topbar-user-dropdown').css('display', 'flex');
+    } else {
+        $('#topbar-user-dropdown').css('display', 'none');
+    }
+});
+
+$(document).on('click', function (e) {
+    if(
+        $(e.target).closest('#topbar-user-dropdown').length == 0 &&
+        $(e.target).closest('#topbar-user-toggle').length == 0 &&
+        userExtended
+    ) {
+        $('#topbar-user-dropdown').css('display', 'none');
+        userExtended = false;
+    }
 });
