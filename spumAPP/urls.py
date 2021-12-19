ba5806b798d2afd360e17df441b6c4b1394d7938
Daniel Lauzurica <daniel.lauzurica@opendeusto.es>
2021-12-19 18:49:16 +0100

    Views a clases

--- spumAPP/urls.py
+++ spumAPP/urls.py
@@ -1,23 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
+from spumAPP.views import *
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home/<int:dashboard_id>', views.home, name='home'),
-    path('dashboard_delete', views.dashboard_delete, name='dashboard_delete'),
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('register', views.register, name='register'),
-    path('settings', views.settings, name='settings'),
-    path('plugins/notes/<int:plugin_id>', views.notes_plugins, name='notes_plugins'),
-    path('plugins/tasks/<int:plugin_id>', views.tasks_plugins, name='tasks_plugins'),
-    path('plugins/tasks/create', views.create_task, name='create_task'),
-    path('plugins/tasks/update', views.update_task, name='update_task'),
-    path('plugins/tasks/delete', views.delete_task, name='delete_task'),
-    path('plugins/notes/create', views.create_note, name='create_note'),
-    path('plugins/notes/update', views.update_note, name='update_note'),
-    path('plugins/notes/delete', views.delete_note, name='delete_note'),
+    path('', IndexView.as_view(), name='index'),
+    path('home/<int:dashboard_id>', login_required(HomeView.as_view()), name='home'),
+    path('dashboard_delete', DashboardDeleteView.as_view(), name='dashboard_delete'),
+    path('register', RegisterView.as_view(), name='register'),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', login_required(LogoutView.as_view()), name='logout'),
+    path('settings', login_required(SettingsView.as_view()), name='settings'),
+    path('plugins/notes/<int:plugin_id>', login_required(NotesPluginsView.as_view()), name='notes_plugins'),
+    path('plugins/tasks/<int:plugin_id>', login_required(TasksPluginsView.as_view()), name='tasks_plugins'),
+    path('plugins/tasks/create', login_required(CreateTaskView.as_view()), name='create_task'),
+    path('plugins/tasks/update', login_required(UpdateTaskView.as_view()), name='update_task'),
+    path('plugins/tasks/delete', login_required(DeleteTaskView.as_view()), name='delete_task'),
+    path('plugins/notes/create', login_required(CreateNoteView.as_view()), name='create_note'),
+    path('plugins/notes/update', login_required(UpdateNoteView.as_view()), name='update_note'),
+    path('plugins/notes/delete', login_required(DeleteNoteView.as_view()), name='delete_note'),
 ]
 
 handler404 = 'spumAPP.views.error_handler_404'
--- spumAPP/views.py
+++ spumAPP/views.py
@@ -1,42 +1,58 @@
-from django.contrib.auth.models import AnonymousUser
-from django.http import HttpResponse, response
+from django.http import HttpResponse
 from .forms import CreateDashboardForm, CreateTaskForm, EditDashboardForm, RegisterForm, LoginForm, CreateNotesForm, EditNotesForm
 from .models import *
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from django.shortcuts import get_list_or_404, get_object_or_404, render, redirect
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 import json
 
 
-def index(request):
-    loggedIn = not request.user.is_anonymous
+class IndexView(View):
+    def get(self, request):
+        loggedIn = not request.user.is_anonymous
     
-    if loggedIn:
+        if loggedIn:
+            user_dashboards = get_list_or_404(Dashboard.objects.filter(user_id=request.user).order_by("pk"));
+            return redirect('home', dashboard_id=user_dashboards[0].id)
+        else:
+            context = {
+                'loggedIn': loggedIn,
+                'user': request.user,
+                'isLanding': True,
+            }
+            return render(request, "index.html", context)
+    
+
+class HomeView(View):
+    def get(self, request, dashboard_id):
+        loggedIn = not request.user.is_anonymous
+    
+        create_form = CreateDashboardForm()
+        edit_form = EditDashboardForm()
+        
         user_dashboards = get_list_or_404(Dashboard.objects.filter(user_id=request.user).order_by("pk"));
-        return redirect('home', dashboard_id=user_dashboards[0].id)
-    else:
+        initial_dashboard = get_object_or_404(Dashboard, pk=dashboard_id)
+        create_form = CreateDashboardForm()
+        edit_form = EditDashboardForm()
+        
         context = {
             'loggedIn': loggedIn,
             'user': request.user,
-            'isLanding': True,
+            'dashboard_list': user_dashboards,
+            'dashboard_list_lenght': len(user_dashboards),
+            'dashboard': initial_dashboard,
+            'create_form': create_form,
+            'edit_form': edit_form,
         }
-        return render(request, "index.html", context)
+        return render(request, "dashboard.html", context)
     
-
-@login_required
-def home(request, dashboard_id):
-    loggedIn = not request.user.is_anonymous
-    
-    create_form = CreateDashboardForm()
-    edit_form = EditDashboardForm()
-    
-    user_dashboards = get_list_or_404(Dashboard.objects.filter(user_id=request.user).order_by("pk"));
-    initial_dashboard = get_object_or_404(Dashboard, pk=dashboard_id)
-    
-    if request.method == "POST":
+    def post(self, request, dashboard_id):
+        initial_dashboard = get_object_or_404(Dashboard, pk=dashboard_id)
+        
         if 'create_dashboard' in request.POST:
             create_form = CreateDashboardForm(request.POST)
             if create_form.is_valid():
@@ -54,37 +70,32 @@ def home(request, dashboard_id):
                 initial_dashboard.save()
                 
                 return redirect('home', dashboard_id=initial_dashboard.id)
-    else:
-        create_form = CreateDashboardForm()
-        edit_form = EditDashboardForm()
+
+
+class DashboardDeleteView(View):
+    def post(self, request):
+        dashboard_id = request.POST.get('dashboard_id')
+        dashboard = get_object_or_404(Dashboard, pk=dashboard_id)
+        dashboard.delete()
+        
+        return redirect('index')
+
+
+class RegisterView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            user_dashboards = get_list_or_404(Dashboard.objects.filter(user_id=request.user).order_by("pk"));
+            return redirect('home', dashboard_id=user_dashboards[0].id)
+        
+        register_form = RegisterForm()
+        context = {
+            'register_form': register_form,
+            'user': request.user,
+            'loggedIn': not request.user.is_anonymous,
+        }
+        return render(request, "register.html", context)
     
-    context = {
-        'loggedIn': loggedIn,
-        'user': request.user,
-        'dashboard_list': user_dashboards,
-        'dashboard_list_lenght': len(user_dashboards),
-        'dashboard': initial_dashboard,
-        'create_form': create_form,
-        'edit_form': edit_form,
-    }
-    return render(request, "dashboard.html", context)
-
-
-@csrf_exempt
-def dashboard_delete(request):
-    dashboard_id = request.POST.get('dashboard_id')
-    dashboard = get_object_or_404(Dashboard, pk=dashboard_id)
-    dashboard.delete()
-    
-    return redirect('index')
-
-
-def register(request):
-    if not request.user.is_anonymous:
-        user_dashboards = get_list_or_404(Dashboard.objects.filter(user_id=request.user).order_by("pk"));
-        return redirect('home', dashboard_id=user_dashboards[0].id)
-    
-    if request.method == 'POST':
+    def post(self, request):
         register_form = RegisterForm(request.POST)
         if register_form.is_valid():
             username = register_form.cleaned_data['username']
@@ -101,18 +112,23 @@ def register(request):
             base_dashboard.save()
             
             return redirect('index')
-    else:
-        register_form = RegisterForm()
-    context = {
-        'register_form': register_form,
-        'user': request.user,
-        'loggedIn': not request.user.is_anonymous,
-    }
-    return render(request, "register.html", context)
+        
 
-
-def login(request):
-    if request.method == 'POST':
+class LoginView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            user_dashboards = get_list_or_404(Dashboard.objects.filter(user_id=request.user).order_by("pk"));
+            return redirect('home', dashboard_id=user_dashboards[0].id)
+        
+        login_form = LoginForm()
+        context = {
+            'login_form' : login_form,
+            'user': request.user,
+            'loggedIn': not request.user.is_anonymous,
+        }
+        return render(request, "login.html", context)
+    
+    def post(self, request):
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
             username = request.POST['username']
@@ -123,74 +139,64 @@ def login(request):
                 return redirect('index')
             else:
                 print('InvalidUser')
-    else:
-        login_form = LoginForm()
-    context = {
-        'login_form' : login_form,
-        'user': request.user,
-        'loggedIn': not request.user.is_anonymous,
-    }
-    return render(request, "login.html", context)
 
 
-@login_required
-def logout(request):
-    if request.user.is_authenticated:
-        django_logout(request)
-    return redirect('index')
+class LogoutView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            django_logout(request)
+        return redirect('index')
 
 
-@login_required
-def settings(request):
-    return HttpResponse("settings.html")
-
-
-@login_required
-def notes_plugins(request, plugin_id):
-    loggedIn = request.user.is_authenticated
-    if (not loggedIn):
-        return redirect('login')
-    notes_plugin = NotesPlugin.objects.get(pk=plugin_id)
-    notes_list = NotesData.objects.filter(plugin_id=plugin_id).order_by('title')
+class SettingsView(View):
+    def get(self, request):
+        return HttpResponse("settings.html")
     
-    create_notes_form = CreateNotesForm()
-    edit_notes_form = EditNotesForm()
-    context = {
-        'user': request.user,
-        'loggedIn': loggedIn,
-        'notes_plugin': notes_plugin,
-        'notes_list': notes_list,
-        'create_notes_form': create_notes_form,
-        'edit_notes_form': edit_notes_form,
-        'plugin_id': plugin_id,
-    }
-    return render(request, "notes.html", context)
+    def post(self, request):
+        return HttpResponse("settings.html")
 
 
-@login_required
-def tasks_plugins(request, plugin_id):
-    loggedIn = request.user.is_authenticated
-    if (not loggedIn):
-        return redirect('login')
-    task_plugin = get_object_or_404(TasksPlugin, pk=plugin_id)
-    task_list = get_list_or_404(TasksData.objects.filter(plugin_id=plugin_id).order_by('title'))
+class NotesPluginsView(View):
+    def get(self, request, plugin_id):
+        loggedIn = request.user.is_authenticated
+        notes_plugin = NotesPlugin.objects.get(pk=plugin_id)
+        notes_list = NotesData.objects.filter(plugin_id=plugin_id).order_by('title')
+        
+        create_notes_form = CreateNotesForm()
+        edit_notes_form = EditNotesForm()
+        context = {
+            'user': request.user,
+            'loggedIn': loggedIn,
+            'notes_plugin': notes_plugin,
+            'notes_list': notes_list,
+            'create_notes_form': create_notes_form,
+            'edit_notes_form': edit_notes_form,
+            'plugin_id': plugin_id,
+        }
+        return render(request, "notes.html", context)
     
-    create_task_form = CreateTaskForm()
-    
-    context = {
-        'user': request.user,
-        'loggedIn': loggedIn,
-        'task_plugin': task_plugin,
-        'task_list': task_list,
-        'create_task_form': create_task_form,
-        'plugin_id': plugin_id,
-    }
-    return render(request, "tasks.html", context)
 
+class TasksPluginsView(View):
+    def get(self, request, plugin_id):
+        loggedIn = request.user.is_authenticated
+        task_plugin = get_object_or_404(TasksPlugin, pk=plugin_id)
+        task_list = get_list_or_404(TasksData.objects.filter(plugin_id=plugin_id).order_by('title'))
+        
+        create_task_form = CreateTaskForm()
+        
+        context = {
+            'user': request.user,
+            'loggedIn': loggedIn,
+            'task_plugin': task_plugin,
+            'task_list': task_list,
+            'create_task_form': create_task_form,
+            'plugin_id': plugin_id,
+        }
+        return render(request, "tasks.html", context)
 
-@csrf_exempt
-def create_note(request):
-    if request.method == 'POST':
+class CreateNoteView(View):
+    @method_decorator(login_required)
+    def post(self, request):
         noteTitle = request.POST.get('noteTitle')
         noteContent = request.POST.get('noteContent')
         plugin_id = request.POST.get('pluginId')
@@ -204,9 +210,9 @@ def create_note(request):
         return HttpResponse(json.dumps(json_data))
 
 
-@csrf_exempt
-def update_note(request):
-    if request.method == 'POST':
+class UpdateNoteView(View):
+    @method_decorator(login_required)
+    def post(self, request):
         noteTitle = request.POST.get('noteTitle')
         content = request.POST.get('noteContent')
         noteID = request.POST.get('noteID')
@@ -219,9 +225,9 @@ def update_note(request):
         return HttpResponse(json.dumps(json_data))
     
     
-@csrf_exempt
-def delete_note(request):
-    if request.method == 'POST':
+class DeleteNoteView(View):
+    @method_decorator(login_required)
+    def post(self, request):
         noteID = request.POST.get('noteID')
         
         note = NotesData.objects.get(pk=noteID)
@@ -230,9 +236,9 @@ def delete_note(request):
         return HttpResponse('success')
 
 
-@csrf_exempt
-def create_task(request):
-    if request.method == 'POST':
+class CreateTaskView(View):
+    @method_decorator(login_required)
+    def post(self, request):
         taskTitle = request.POST.get('taskTitle')
         taskPriority = request.POST.get('taskPriority')
         plugin_id = request.POST.get('pluginId')
@@ -246,9 +252,9 @@ def create_task(request):
         return HttpResponse(json.dumps(json_data))
 
 
-@csrf_exempt
-def update_task(request):
-    if request.method == 'POST':
+class UpdateTaskView(View):
+    @method_decorator(login_required)
+    def post(self, request):
         isDone = request.POST.get('isDone') == 'true'
         taskID = request.POST.get('taskID')
         
@@ -257,18 +263,19 @@ def update_task(request):
         task.save()
 
         return HttpResponse('success')
-    
-    
-@csrf_exempt
-def delete_task(request):
-    if request.method == 'POST':
+
+
+class DeleteTaskView(View):
+    @method_decorator(login_required)
+    def post(self, request):
         taskID = request.POST.get('taskID')
         
         task = TasksData.objects.get(pk=taskID)
         task.delete()
 
         return HttpResponse('success')
-    
-    
+
+
+# TODO: basar esto en clase?
 def error_handler_404(request, exception):
     return render(request, '404.html', status=404)
